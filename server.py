ac0e6d62678fa4d0e9787e7977f506313f0cf993
alexfilipe <alex@afcbs.me>
2019-11-04 16:46:03 -0500

    add HTTP response class

--- server.py
+++ server.py
@@ -1,17 +1,73 @@
 import re
 import socket
-
+from email.utils import formatdate
 
 class OperationIncomplete(ValueError):
     pass
 
-
 class InvalidOperation(ValueError):
     pass
 
-
-class Status300(SystemError):
+class Status406(SystemError):
     pass
+
+class HTTPRequest:
+    pass
+
+class HTTPResponse:
+    def __init__(self):
+        self.http_version = "HTTP/1.1"
+
+        self.status_codes = {
+            200: "200 OK",
+            406: "406 Not Acceptable"
+        }
+
+        self.content_type = "text/plain"
+
+        self.server = "calculator/0.1"
+
+        self.response_template = (
+            "{version} {{status}}\n"
+            "Date: {{date}}\n"
+            "Content-Type: {content_type}\n"
+            "Server: {server}\n"
+            "\n{{data}}"
+        )
+
+        self.response_template = self.response_template.format(
+            version=self.http_version,
+            server=self.server,
+            content_type=self.content_type
+        )
+
+    def gmt_date(self):
+        """Return current datetime in GMT format."""
+        return formatdate(timeval=None, localtime=False, usegmt=True)
+
+    def build_200(self, data: str) -> str:
+        """Build 200 OK response."""
+        if data is None:
+            data = ""
+
+        response = self.response_template.format(
+            status=self.status_codes[200],
+            date=self.gmt_date(),
+            data=data
+        )
+
+        return response
+
+    def build_response(self, data: str, status: int=200) -> str:
+        """Build the HTTP response."""
+        response = ""
+
+        if status == 200:
+            response = self.build_200(data)
+        elif status == 300:
+            pass
+
+        return response
 
 
 class Calculator:
@@ -73,7 +129,7 @@ class Server:
         self.host = host
         self.port = port
 
-    def runServer(self):
+    def run_server(self):
         pass
 
 
@@ -86,7 +142,11 @@ class TCPServer(Server):
 
 
 if __name__ == "__main__":
+    response = HTTPResponse()
+
+    print(response.build_response(status=200, data='hello'))
+
     calc = Calculator()
     print('Result:', calc.evaluate("+  1   3"))
     print('Result:', calc.evaluate("/ 1   3"))
-    print('Result:', calc.evaluate("/ 1 0"))
+    # print('Result:', calc.evaluate("/ 1 0"))
